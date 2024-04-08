f4a45fa323c3ad7460d2137c137574b0162e1129
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-04-08 23:12:31 +0200

    Add logic for updateCar, still not complete

--- uek294_cars-project/src/Service/CarDataService.ts
+++ uek294_cars-project/src/Service/CarDataService.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from "axios";
+import { AxiosInstance } from "axios";
 import { defaultAxiosInstance } from "./Api";
 import { login } from "./Authorization";
 
@@ -62,6 +62,29 @@ const CarService = (api: AxiosInstance = defaultAxiosInstance) => ({
       const data = { Name, Year };
 
       const response = await api.post("cars", data, config);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  updateCar: async (Name, Year) => {
+    try {
+      await login("maximilian@mail.com", "1234");
+      const accessToken = localStorage.getItem("accessToken");
+      if (!accessToken) {
+        throw new Error("No Access token in local storage available");
+      }
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      };
+
+      const data = { Name, Year };
+
+      const response = await api.put("cars", data, config);
       return response.data;
     } catch (error) {
       throw error;
