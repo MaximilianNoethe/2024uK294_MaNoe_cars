d19b197629f79a6ed22510720bedd26d6fbd7932
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-04-09 17:55:47 +0200

    Update appearance of DetailedCarPage, no Cards will be used and logic to load data from selected car is added

--- uek294_cars-project/src/components/Pages/DetailedCarPage.tsx
+++ uek294_cars-project/src/components/Pages/DetailedCarPage.tsx
@@ -2,77 +2,67 @@
 import Navbar from "../Molecules/Navbar";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import CarService from "../../Service/CarDataService";
 
 function DetailedCarPage() {
+  const { carId } = useParams(); // Gets carId from URL params
+  const [name, setName] = useState("");
+  const [year, setYear] = useState("");
+
+  useEffect(() => {
+    // Function to load data from car with the selected carId
+    const fetchData = async () => {
+      try {
+        const carData = await CarService().getCarById(carId);
+        setName(carData.Name);
+        setYear(carData.Year);
+      } catch (error) {
+        alert("Error fetching car data.");
+      }
+    };
+
+    fetchData();
+  }, [carId]);
+
   return (
     <>
-     <Navbar />
+      <Navbar />
       <h1>Detailed version</h1>
-      <Card className="cardWrapper" sx={{ maxWidth: 500 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={6}>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={books.image}
-                  sx={{ height: 270 }}
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    <TextField
-                      id="standard-required-read-only-input"
-                      label="Bookname"
-                      defaultValue={books.name}
-                      InputProps={{
-                        readOnly: true,
-                      }}
-                      variant="standard"
-                    />
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    <TextField
-                      onChange={handleChange}
-                      id="outlined-required-read-only-input"
-                      label="Author"
-                      defaultValue={books.author}
-                      InputProps={{
-                        readOnly: true,
-                      }}
-                    />
-                    <br />
-                    <br />
-                    <TextField
-                      onChange={handleChange}
-                      id="outlined-required-read-only-input"
-                      label="Genre"
-                      defaultValue={books.genre}
-                      InputProps={{
-                        readOnly: true,
-                      }}
-                    />
-                    <br />
-                    <br />
 
-                    <TextField
-                      onChange={handleChange}
-                      id="outlined-required-read-only-input"
-                      label="Price"
-                      defaultValue={books.price}
-                      InputProps={{
-                        readOnly: true,
-                      }}
-                    />
-                  </Typography>
-                </CardContent>
-              </Grid>
-            </Grid>
-          </Card>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              <TextField
+                id="standard-required-read-only-input"
+                label="Carname"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                InputProps={{
+                  readOnly: true,
+                }}
+                variant="standard"
+              />
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              <TextField
+                id="outlined-required-read-only-input"
+                label="Year"
+                value={year}
+                onChange={(e) => setYear(e.target.value)}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
     </>
   );
 }
