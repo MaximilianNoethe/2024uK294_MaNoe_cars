065695890d0224ec4caa7b0dd7e618597444f6f3
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-04-10 15:29:23 +0200

    Format CreateCar.tsx

--- uek294_cars-project/src/components/Organism/CreateCar.tsx
+++ uek294_cars-project/src/components/Organism/CreateCar.tsx
@@ -24,40 +24,36 @@ function CreateCar() {
     <>
       <Navbar />
       <h1>Create a new car</h1>
-      
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-           
-              <Typography>
-                <TextField
-                  id="margin-normal"
-                  label="Name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)} // Rendered the whole site and checks if something changed, if so then it sets the new data
-                  variant="standard"
-                />
-                <br />
-                <TextField
-                  id="margin-normal"
-                  label="Year"
-                  // type="date"
-                  value={year}
-                  onChange={(e) => setYear(e.target.value)}
-                  variant="standard"
-                />
-              </Typography>
-              <Button
-                variant="contained"
-                onClick={handleCreateCar}
-                component={Link}
-                to="/car"
-              >
-                Create
-              </Button>
-           
-          </Grid>
+
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Typography>
+            <TextField
+              id="margin-normal"
+              label="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)} // Rendered the whole site and checks if something changed, if so then it sets the new data
+              variant="standard"
+            />
+            <br />
+            <TextField
+              id="margin-normal"
+              label="Year"
+              value={year}
+              onChange={(e) => setYear(e.target.value)}
+              variant="standard"
+            />
+          </Typography>
+          <Button
+            variant="contained"
+            onClick={handleCreateCar}
+            component={Link}
+            to="/car"
+          >
+            Create
+          </Button>
         </Grid>
-      
+      </Grid>
     </>
   );
 }
