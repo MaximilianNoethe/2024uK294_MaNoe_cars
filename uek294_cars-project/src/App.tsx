bba4a615c4bb9213dc9f28b38d6ba1874b7bb39a
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-04-10 10:48:03 +0200

    Update App.tsx

--- uek294_cars-project/src/App.tsx
+++ uek294_cars-project/src/App.tsx
@@ -8,14 +8,13 @@ import DetailedCarPage from "./components/Pages/DetailedCarPage";
 function App() {
   return (
     <>
-    
       <BrowserRouter>
         <Routes>
           <Route path="car" Component={CarPage} />
+          <Route path="car/:carId" Component={DetailedCarPage} />
           <Route path="login" Component={LoginPage} />
           <Route path="post" Component={CreateCarPage} />
           <Route path="update/:carId" Component={EditCarPage} />
-          <Route path="details" Component={DetailedCarPage} />
         </Routes>
       </BrowserRouter>
     </>
